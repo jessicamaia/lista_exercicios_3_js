79119caac342fec175385fc2c07fd38ff64b3a19
jessicamaia <51716788+jessicamaia@users.noreply.github.com>
2019-12-12 22:44:37 -0300

    Resolução da sétima questão.

---
+++ Questao07/js/script.js
@@ -0,0 +1,20 @@
+let nome= ""
+let NNome
+let SemESpaco
+ 
+function replaceAll(str, de, para){
+    var pos = str.indexOf(de);
+    while (pos > -1){
+		str = str.replace(de, para);
+		pos = str.indexOf(de);
+	}
+    return (str);
+}
+
+nome= prompt ('Por favor insira seu nome completo:' ,"");
+NNome= nome.length;
+
+NomeSemEspaco= replaceAll (nome,' ', "");
+SemEspaco=NomeSemEspaco.length
+
+alert(SemEspaco);
