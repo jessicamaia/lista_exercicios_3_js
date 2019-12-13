c49b0548384cf8109eb7776865066aec75f63495
jessicamaia <51716788+jessicamaia@users.noreply.github.com>
2019-12-12 22:38:49 -0300

    Resolução da quarta questão.

---
+++ Questao04/js/script.js
@@ -0,0 +1,16 @@
+let idade=0; 
+let MaiorDeIdade=0; 
+let MenorDeIdade=0;
+
+while (idade  != 'fim') {
+    idade= prompt ('Por favor insira uma idade:' ,"");
+    if (idade >=18){
+        MaiorDeIdade++;
+    } 
+    if(idade <18){
+        MenorDeIdade++;
+    }
+
+}
+
+alert ('Quantidade inserida de pessoas maiores de idade: '+ MaiorDeIdade + "\n" + 'Quantidade inserida de pessoas menores de idade: '+ MenorDeIdade);
