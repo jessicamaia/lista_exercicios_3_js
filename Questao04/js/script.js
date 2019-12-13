541c68441abf2d9b5d17b6d31af6d9c504d9c64f
jessicamaia <51716788+jessicamaia@users.noreply.github.com>
2019-12-13 20:46:23 -0300

    Update script.js

--- Questao04/js/script.js
+++ Questao04/js/script.js
@@ -4,13 +4,16 @@ let MenorDeIdade=0;
 
 while (idade  != 'fim') {
     idade= prompt ('Por favor insira uma idade:' ,"");
+    if (idade=="fim"){
+        break;
+    }
+    idade=Number (idade) 
     if (idade >=18){
         MaiorDeIdade++;
     } 
     if(idade <18){
         MenorDeIdade++;
     }
-
 }
 
 alert ('Quantidade inserida de pessoas maiores de idade: '+ MaiorDeIdade + "\n" + 'Quantidade inserida de pessoas menores de idade: '+ MenorDeIdade);
