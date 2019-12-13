94ccb441f29c75d12389c81a11180ff5dda35888
jessicamaia <51716788+jessicamaia@users.noreply.github.com>
2019-12-13 14:46:31 -0300

    Resolução da nona questão.

---
+++ Questão09/js/script.js
@@ -0,0 +1,28 @@
+let Nome
+let NomeCompleto=0;
+let Resultado
+let Numero;
+
+NomeCompleto=prompt('Por favor insira o nome completo: ',"");
+Numero= prompt('Por favor insira um número: ',"");
+
+function validacao(Nome, Numero){
+    if(Numero > Nome.length){
+        alert("Inválido.");
+        return;
+    }
+    if(Numero == 0){
+        alert("Inválido.");
+        return;
+    }
+    if(Numero <= -1){
+        alert("Inválido.");
+        return;
+    }
+    
+    return Nome.charAt(Numero-1);
+}
+
+Resultado=validacao(NomeCompleto,Numero);
+
+alert("O caractere " + Resultado + " corresponde ao número da posicao informada.");
